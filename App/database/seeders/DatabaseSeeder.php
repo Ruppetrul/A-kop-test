444509ed215d92faa99104da9b252c331b733786
serg <nautbek@gmail.com>
2024-04-13 14:12:48 +0300

    Fix user table migration and DatabaseSeeder.php

--- App/database/seeders/DatabaseSeeder.php
+++ App/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $admin = User::create([
+            'name'     => 'Иван',
+            'surname'  => 'Иванов',
+            'email'    => 'admin@admin.com',
+            'password' => Hash::make('admin'),
+        ]);
     }
 }
