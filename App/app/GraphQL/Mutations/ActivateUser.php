7e2a46620c723418402ea259d03931fdd1c06944
serg <nautbek@gmail.com>
2024-04-13 22:55:41 +0300

    Add add activate user endpoint.

---
+++ App/app/GraphQL/Mutations/ActivateUser.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\GraphQL\Mutations;
+
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class ActivateUser
+{
+    public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+        //TODO
+    }
+}
