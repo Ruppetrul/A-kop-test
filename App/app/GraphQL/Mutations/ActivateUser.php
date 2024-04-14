b75c15a7135d77447ee761c088ded53ac3ab5edb
serg <nautbek@gmail.com>
2024-04-14 12:58:45 +0300

    Implement activate endpoint.

--- App/app/GraphQL/Mutations/ActivateUser.php
+++ App/app/GraphQL/Mutations/ActivateUser.php
@@ -2,13 +2,37 @@
 
 namespace App\GraphQL\Mutations;
 
+use App\Models\Invitation;
+use App\Models\User;
 use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\Hash;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
 class ActivateUser
 {
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
-        //TODO
+        $userEmail = $args['input']['email'];
+        $jwt       = $args['input']['jwt'];
+        $name      = $args['input']['name'];
+        $surname   = $args['input']['surname'];
+        $password  = $args['input']['password'];
+
+        $invitation = Invitation::find([
+            'email' => $userEmail,
+            'jwt'   => $jwt,
+        ]);
+
+        if (!$invitation) {
+            throw new \Exception("Invitation with such email adn jwt token not found.");
+        }
+
+        $user = User::firstOrNew(['email' => $userEmail]);
+        if (!$user->password) {
+            $user->name = $name;
+            $user->surname = $surname;
+            $user->password = Hash::make($password);
+            $user->save();
+        }
     }
 }
