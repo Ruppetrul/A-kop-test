9527987240ab41c4ad3836c8af42a73781e60d5f
serg <nautbek@gmail.com>
2024-04-14 20:22:29 +0300

    Add roles in login.

--- App/app/GraphQL/Mutations/Login.php
+++ App/app/GraphQL/Mutations/Login.php
@@ -2,16 +2,20 @@
 
 namespace App\GraphQL\Mutations;
 
+use Exception;
 use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
-class loginUser
+class login
 {
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
         $code = 200;
+        $roles = [];
         $jwtToken = null;
 
         $email = $args['input']['email'];
@@ -25,19 +29,37 @@ class loginUser
                 'password' => $password,
             ];
             if (!$jwtToken = JWTAuth::attempt($credentials)) {
-                return response()->json(['error' => 'Unauthorized'], 401);
+                throw new AuthorizationException();
+            }
+
+            $user = JWTAuth::user();
+            $userCompanies = $user->companies()->get();
+
+            $roleData = [];
+            foreach($userCompanies as $company) {
+                $data = $company['pivot'];
+                $roleData[$data['company_id']][] = $data['role_id'];
+            }
+
+            foreach ($roleData as $company => $roleIds) {
+                $roles[] = ['company' => $company, 'roles' => $roleIds];
             }
 
             DB::commit();
-        } catch (\Exception $e) {
-            DB::rollBack();
+        } catch (AuthorizationException $e) {
+            $code = 401;
+            $jwtToken = null;
+        } catch (Exception $e) {
             $code = 500;
             $jwtToken = null;
+        } finally {
+            DB::rollBack();
         }
 
         return [
             'token' => $jwtToken,
             'code'  => $code,
+            'roles' => $roles,
         ];
     }
 }
