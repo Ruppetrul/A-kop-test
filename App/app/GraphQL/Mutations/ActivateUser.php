249165a7653d6b6e3cfabd77ac190a110cea954e
serg <nautbek@gmail.com>
2024-04-14 18:03:11 +0300

    Fix activateUser.

--- App/app/GraphQL/Mutations/ActivateUser.php
+++ App/app/GraphQL/Mutations/ActivateUser.php
@@ -5,6 +5,7 @@ namespace App\GraphQL\Mutations;
 use App\Models\Invitation;
 use App\Models\User;
 use GraphQL\Type\Definition\ResolveInfo;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 
@@ -12,27 +13,46 @@ class ActivateUser
 {
     public function __invoke($rootValue, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
     {
+        $code = 200;
+
         $userEmail = $args['input']['email'];
         $jwt       = $args['input']['jwt'];
         $name      = $args['input']['name'];
         $surname   = $args['input']['surname'];
         $password  = $args['input']['password'];
 
-        $invitation = Invitation::find([
-            'email' => $userEmail,
-            'jwt'   => $jwt,
-        ]);
+        try {
+            DB::beginTransaction();
+            $invitation = Invitation::find([
+                'email' => $userEmail,
+                'jwt'   => $jwt,
+            ]);
 
-        if (!$invitation) {
-            throw new \Exception("Invitation with such email adn jwt token not found.");
+            if (!$invitation) {
+                throw new \Exception("Invitation with such email adn jwt token not found.");
+            }
+
+            $user = User::firstOrNew(['email' => $userEmail]);
+            if (!$user) {
+                throw new \Exception("User not found.");
+            }
+
+            if ($user->password) {
+                throw new \Exception("User has already been activated.");
+            } else {
+                $user->name = $name;
+                $user->surname = $surname;
+                $user->password = Hash::make($password);
+                $user->save();
+            }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $code = 500;
         }
 
-        $user = User::firstOrNew(['email' => $userEmail]);
-        if (!$user->password) {
-            $user->name = $name;
-            $user->surname = $surname;
-            $user->password = Hash::make($password);
-            $user->save();
-        }
+        return [
+            'code' => $code
+        ];
     }
 }
